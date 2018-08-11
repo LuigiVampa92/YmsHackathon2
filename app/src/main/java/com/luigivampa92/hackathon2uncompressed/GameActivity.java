99e7e9bff487008ed2cfd2d8ae118b342a546b77
Pavel Vasiliev <luigivampa92@gmail.com>
2018-08-11 16:13:40 +0300

    removed appcompat

--- app/src/main/java/com/luigivampa92/hackathon2uncompressed/GameActivity.java
+++ app/src/main/java/com/luigivampa92/hackathon2uncompressed/GameActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class GameActivity extends Activity implements View.OnClickListener {
 
--- app/src/main/java/com/luigivampa92/hackathon2uncompressed/MainActivity.java
+++ app/src/main/java/com/luigivampa92/hackathon2uncompressed/MainActivity.java
@@ -1,11 +1,10 @@
 package com.luigivampa92.hackathon2uncompressed;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
