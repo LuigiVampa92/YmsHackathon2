58a518d074f9fba97917b7689984ddad1e358961
beardie <iliailemkov@gmail.com>
2018-08-11 18:31:38 +0300

    background

--- app/src/main/java/com/luigivampa92/hackathon2uncompressed/GameActivity.java
+++ app/src/main/java/com/luigivampa92/hackathon2uncompressed/GameActivity.java
@@ -28,7 +28,7 @@ public class GameActivity extends Activity implements View.OnClickListener {
         mGameView.setBackgroundColor(Color.TRANSPARENT);
         backgroundView = findViewById(R.id.background);
         animator = new GameLoop(mGameView, FPS);
-        backgorundAnimator = new BackgorundLoop(backgroundView, FPS);
+        backgorundAnimator = new BackgorundLoop(backgroundView, 20);
         Log.d("GameView", "gv object: " + mGameView);
 
 //        mGameView.mLivesTextView = (TextView) findViewById(R.id.livesLabel);
